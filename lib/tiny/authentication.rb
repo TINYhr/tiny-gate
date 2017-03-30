f9211176b51628ace022c8a97686980e92ddb648
Hieu Nguyen <hieuk09@gmail.com>
2017-03-30 15:55:28 +0700

    Initial commit

---
+++ lib/tiny/authentication.rb
@@ -0,0 +1,7 @@
+require "tiny/authentication/version"
+
+module Tiny
+  module Authentication
+    # Your code goes here...
+  end
+end
---
+++ lib/tiny/authentication/version.rb
@@ -0,0 +1,5 @@
+module Tiny
+  module Authentication
+    VERSION = "0.1.0"
+  end
+end
