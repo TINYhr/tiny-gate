a7af96b159862d6866a0e20db431dee4adabd4a1
Hieu Nguyen <hieuk09@gmail.com>
2017-03-31 16:32:45 +0700

    [f] Add spec

---
+++ spec/tiny_gate/types/session_response_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe Types::SessionResponse do
+  describe '#success?' do
+    let(:response) { double('Response', status: status, body: '{}') }
+    let(:session_response) { described_class.new(response) }
+    subject { session_response.success? }
+
+    context 'when response status is 200' do
+      let(:status) { 200 }
+      it { is_expected.to eq true }
+    end
+
+    context 'when response status is not 200' do
+      let(:status) { 400 }
+      it { is_expected.to eq false }
+    end
+  end
+end
